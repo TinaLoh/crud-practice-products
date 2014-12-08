20f16292df833e0a858b51b0bb4e4506bb599937
TinaLoh <tinalola@gmail.com>
2014-12-08 12:45:39 -0800

    finish commit

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,56 @@
+class ProductsController < ApplicationController
+
+  def index
+    @products = Product.all
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+      if @product.save
+      flash[:notice] = "Product was created successfully"
+      redirect_to products_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    @product.update(product_params)
+    if @product.save
+      flash[:notice] = "Product was updated successfully"
+      redirect_to products_path
+    else
+      render :edit
+    end
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to products_path
+  end
+
+private
+
+  def product_params
+    params.require(:product).permit(
+    :name,
+    :description,
+    :price,
+    )
+  end
+
+end
